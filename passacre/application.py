83df2dfa015f7077e24d570da6e5ace0fcca8532
Aaron Gallagher <habnabit@gmail.com>
2013-01-11 22:29:59 -0800

    Improving/documenting the passacre application.

--- passacre/application.py
+++ passacre/application.py
@@ -5,21 +5,45 @@ import argparse
 from getpass import getpass
 import math
 import pprint
+import sys
 
 class Passacre(object):
-    subcommands = 'generate', 'entropy'
+    _subcommands = {
+        'generate': "generate a password",
+        'entropy': "display each site's password entropy",
+    }
 
     def load_config(self):
+        "Load the passacre configuration to ``self.config``."
         self.config = load_config(open('config.yaml'))
 
+    def generate_args(self, subparser):
+        subparser.add_argument('site', nargs='?',
+                               help='site for which to generate a password')
+        subparser.add_argument('-n', '--no-newline', action='store_true',
+                               help="don't write a newline after the password")
+        subparser.add_argument('-c', '--confirm', action='store_true',
+                               help='confirm prompted password')
+
     def generate_action(self, args):
+        "Generate a password."
         password = getpass()
-        if password != getpass('Confirm password: '):
+        if args.confirm and password != getpass('Confirm password: '):
             raise ValueError("passwords don't match")
-        site = raw_input('Site: ')
-        print generate_from_config(password, site, self.config)
+        if args.site is None:
+            sys.stderr.write('Site: ')
+            args.site = raw_input()
+        sys.stdout.write(generate_from_config(password, args.site, self.config))
+        if not args.no_newline:
+            sys.stdout.write('\n')
 
     def entropy_action(self, args):
+        """Display each site's potential password entropy in bits.
+
+        This is the same thing as the base 2 logarithm of the number of
+        possible passwords for a site.
+        """
+
         entropy = [
             (site, math.log(site_config['multibase'].max_encodable_value + 1, 2))
             for site, site_config in self.config.iteritems()
@@ -27,14 +51,17 @@ class Passacre(object):
         pprint.pprint(entropy)
 
     def build_parser(self):
-        parser = argparse.ArgumentParser()
+        "Build an ``ArgumentParser`` from the defined subcommands."
+        parser = argparse.ArgumentParser(prog='passacre')
         subparsers = parser.add_subparsers()
-        for subcommand in self.subcommands:
-            subparser = subparsers.add_parser(subcommand)
+        for subcommand, subcommand_help in self._subcommands.iteritems():
+            action_method = getattr(self, '%s_action' % (subcommand,))
+            subparser = subparsers.add_parser(
+                subcommand, help=subcommand_help, description=action_method.__doc__)
             args_method = getattr(self, '%s_args' % (subcommand,), None)
             if args_method is not None:
                 args_method(subparser)
-            subparser.set_defaults(func=getattr(self, '%s_action' % (subcommand,)))
+            subparser.set_defaults(func=action_method)
         return parser
 
     def main(self, args=None):
