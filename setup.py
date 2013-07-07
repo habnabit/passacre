72b9d0e18be3119de2e799220b852a9fc6f66baf
Aaron Gallagher <habnabit@gmail.com>
2013-07-06 20:33:06 -0700

    Update setup.py and .gitignore; use vcversioner.

--- setup.py
+++ setup.py
@@ -4,14 +4,17 @@
 # may god have mercy on my soul
 from setuptools import setup
 
+
 setup(
     name='passacre',
+    vcversioner={},
     packages=['passacre', 'passacre.test'],
+    setup_requires=['vcversioner'],
     extras_require={
         'config': ['PyYAML'],
         'clipboard': ['xerox'],
         'keccak-generation': ['cykeccak>=0.13.2'],
-        'skein-generation': ['pyskein'],
+        'skein-generation': ['pyskein>=0.7'],
     },
     entry_points={
         'console_scripts': ['passacre = passacre.application:main [config]'],
