e007809a04d553af0c554f66c0516e4c1e74462a
Aaron Gallagher <habnabit@gmail.com>
2013-07-08 09:23:37 -0700

    Add word-based password generation.

--- passacre/config.py
+++ passacre/config.py
@@ -7,6 +7,7 @@ from passacre.multibase import MultiBase
 
 import string
 import yaml
+import os
 
 default_digits = {
     'printable': string.digits + string.ascii_letters + string.punctuation,
@@ -17,17 +18,31 @@ default_digits = {
     'uppercase': string.ascii_uppercase,
 }
 
-def multibase_of_schema(schema):
+def multibase_of_schema(schema, words):
     "Convert a password schema from decoded YAML to a ``MultiBase``."
     ret = []
     for item in schema:
-        count = 1
-        if isinstance(item, list) and isinstance(item[0], int):
-            count, item = item
-        if not isinstance(item, list):
-            item = [item]
-        item = ''.join(default_digits.get(i, i) for i in item)
-        ret.extend([item] * count)
+        if isinstance(item, list) and item[1] == 'word':
+            count = item[0]
+            delimiter = ' '
+            if len(item) == 3:
+                delimiter = item[2]
+            if not words:
+                raise ValueError('no words provided')
+            items = []
+            for x in xrange(count):
+                if x != 0:
+                    items.append([delimiter])
+                items.append(words)
+        else:
+            count = 1
+            if isinstance(item, list) and isinstance(item[0], int):
+                if len(item) == 2:
+                    count, item = item
+            if not isinstance(item, list):
+                item = [item]
+            items = [''.join(default_digits.get(i, i) for i in item)] * count
+        ret.extend(items)
     return MultiBase(ret)
 
 def load(infile):
@@ -37,11 +52,18 @@ def load(infile):
     defaults.setdefault('method', 'keccak')
     defaults.setdefault('iterations', 1000)
 
+    if 'words-file' in parsed:
+        with open(os.path.expanduser(parsed['words-file'])) as infile:
+            words = [word.strip() for word in infile]
+    else:
+        words = None
+
     sites = {}
     for site, additional_config in parsed['sites'].items():
         site_config = sites[site] = defaults.copy()
         site_config.update(additional_config)
-        site_config['multibase'] = multibase_of_schema(site_config['schema'])
+        site_config['multibase'] = multibase_of_schema(
+            site_config['schema'], words)
         site_config['iterations'] = (
             site_config.get('iterations', 1000) + site_config.get('increment', 0))
 
