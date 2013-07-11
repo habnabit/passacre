aea871423ecb268001a0f34eacc68af0a8cc9cc6
Aaron Gallagher <habnabit@gmail.com>
2013-07-10 19:38:41 -0700

    Add a signing UUID for ssh-agent stuff.

--- passacre/__init__.py
+++ passacre/__init__.py
@@ -1,4 +1,11 @@
 # Copyright (c) Aaron Gallagher <_@habnab.it>
 # See COPYING for details.
 
+import uuid
+
 from passacre._version import __version__, __sha__
+
+
+signing_uuid = uuid.UUID('dd34b62f-9ed5-597e-85a2-c15d48ed6832')
+
+__all__ = ['__version__', '__sha__', 'signing_uuid']
