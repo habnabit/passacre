72a8b55d8b4701ca4c3e770d887d162a6bb3794b
Aaron Gallagher <_@habnab.it>
2015-04-16 18:09:01 -0400

    Use cryptography's travis build matrix technique.
    Travis build matrixes are kind of wacky, so let's just programmatically
    generate the matrix ourselves instead of relying on travis to get it
    right.
    
    Also because only 'language: python' has the right pythons, switch back
    to that.

---
+++ admin/travis-build-matrix.py
@@ -0,0 +1,16 @@
+print '''
+matrix:
+  include:
+'''
+
+for compiler in ['clang', 'gcc']:
+    for python in ['2.6', '2.7', '3.2', '3.3', '3.4']:
+        print '''
+    - python: {pydot}
+      env: TOXENV=py{pynodot},coveralls CC={compiler}
+'''.format(compiler=compiler, pydot=python, pynodot=python.replace('.', ''))
+
+print '''
+    - python: 2.7
+      env: TOXENV=gcovr,cpp-coveralls CC=clang
+'''
