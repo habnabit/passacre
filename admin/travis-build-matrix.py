442f6e94e78584170c2fc62614edd029ba4ce4e4
Aaron Gallagher <_@habnab.it>
2015-04-19 22:48:11 -0700

    Fix the travis builds.
    This will /actually/ use clang for coverage analysis. It turns out that
    travis's builtin clang is turbofucked and not actually usable, and you
    have to install the clang-3.4 package to get something usable. After
    that, it's okay to use clang as the linker, and using clang as the
    linker with --coverage produces coverage-tracking shared objects!
    
    --coverage is used instead of the -f flags because it enables everything
    you need easier. Also it turns out that distutils will use CFLAGS for
    linking too so LDFLAGS isn't necessary.

--- admin/travis-build-matrix.py
+++ admin/travis-build-matrix.py
@@ -7,10 +7,10 @@ for compiler in ['clang', 'gcc']:
     for python in ['2.6', '2.7', '3.2', '3.3', '3.4']:
         print '''
     - python: {pydot}
-      env: TOXENV=py{pynodot},coveralls CC={compiler}
+      env: TOXENV=py{pynodot},coveralls _COMPILER={compiler}
 '''.format(compiler=compiler, pydot=python, pynodot=python.replace('.', ''))
 
 print '''
     - python: 2.7
-      env: TOXENV=gcovr,cpp-coveralls CC=clang
+      env: TOXENV=gcovr,cpp-coveralls _COMPILER=clang
 '''
