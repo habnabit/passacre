a433628f62d30cbbed39b9091b01bd22e7350c4d
Aaron Gallagher <_@habnab.it>
2015-10-18 20:03:42 -0700

    Switch to codecov.

--- admin/travis-build-matrix.py
+++ admin/travis-build-matrix.py
@@ -7,5 +7,5 @@ for compiler in ['clang', 'gcc']:
     for python in ['2.6', '2.7', '3.3', '3.4', '3.5']:
         print '''
     - python: {pydot}
-      env: TOXENV=py{pynodot},coveralls _COMPILER={compiler}
+      env: TOXENV=py{pynodot} _COMPILER={compiler}
 '''.format(compiler=compiler, pydot=python, pynodot=python.replace('.', ''))
