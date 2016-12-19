06359be6a47b18d29d547e3cd2c255ad77856dd0
Aaron Gallagher <_@habnab.it>
2016-12-18 20:22:13 -0800

    Restore python 2.6.

--- passacre/_backend_capnp.py
+++ passacre/_backend_capnp.py
@@ -22,10 +22,11 @@ class SubprocessClient(object):
         if self._proc is not None:
             self._proc.wait()
         self._sock, remote = socket.socketpair()
-        with closing(remote), open(os.devnull, 'r+') as devnull:
-            self._proc = subprocess.Popen(
-                [self._backend, str(remote.fileno())],
-                stdin=devnull, pass_fds=[remote.fileno()])
+        with closing(remote):
+            with open(os.devnull, 'r+') as devnull:
+                self._proc = subprocess.Popen(
+                    [self._backend, str(remote.fileno())],
+                    stdin=devnull, pass_fds=[remote.fileno()])
         self._sock_client = capnp.TwoPartyClient(self._sock)
         self._client = self._sock_client.bootstrap().cast_as(_passacre_capnp.Toplevel)
 
