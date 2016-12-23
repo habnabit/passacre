47fd47fa776bb5465a79dda3e22a4b47fccf041e
Aaron Gallagher <_@habnab.it>
2016-12-22 23:14:23 -0800

    Refresh the git index before building wheels.
    This is required or it'll always be a -dirty version.

--- docker/build-linux-backend.sh
+++ docker/build-linux-backend.sh
@@ -1,4 +1,9 @@
 #!/bin/bash -lex
+
+# I guess git is picky about caching stat info.
+cd /build
+git update-index --refresh
+
 mkdir -p ~/build /build/wheelhouse
 cd ~/build
 cmake /build/passacre-backend -DCMAKE_BUILD_TYPE=Release
